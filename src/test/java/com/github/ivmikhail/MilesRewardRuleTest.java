9dcfd27e8f57ae99ea4122315735d3717a3c314d
Mikhail Ivanov <ivmikhail@gmail.com>
2018-02-07 14:47:00 +0300

    Fixed tests

--- src/test/java/com/github/ivmikhail/MilesRewardRuleTest.java
+++ src/test/java/com/github/ivmikhail/MilesRewardRuleTest.java
@@ -34,72 +34,36 @@ public class MilesRewardRuleTest {
         transaction.setProcessedDate(LocalDate.now());
         transaction.setAmount(new BigDecimal("-950"));
         transaction.setCurrencyCode("RUR");
+        transaction.setAccountCurrencyCode("RUR");
         transaction.setAmountInAccountCurrency(new BigDecimal("-950"));
         transaction.setDescription("UBER RU JUN29 ABCD HELP;");
         transaction.setStatus("Исполнено");
     }
 
-//    @Test
-//    public void test() {
-//        transaction.setAmountInAccountCurrency(new BigDecimal("-100.0"));
-//        RewardResult result = rule.process(Collections.singletonList(transaction));
-//
-//        assertEquals(1, result.getTransactionsMap().values().size());
-//        assertEquals(0, result.getBalanceRUR().compareTo(new BigDecimal("100.0")));
-//
-//    }
-
-//    @Test
-//    public void testLessThan100() {
-//        transaction.setAmountInAccountCurrency(new BigDecimal("-95.0"));
-//        RewardResult result = rule.process(Collections.singletonList(transaction));
-//
-//        assertTrue(result.isWithdraw());
-//        assertFalse(result.isIgnore());
-//        assertFalse(result.isForeign());
-//        assertEquals(0, result.getMiles().compareTo(new BigDecimal("0")));
-//
-//    }
-
-//    @Test
-//    public void testRounding() {
-//        transaction.setAmountInAccountCurrency(new BigDecimal("-195.0"));
-//        RewardResult result = rule.process(Collections.singletonList(transaction));
-//
-//
-//        assertTrue(result.isWithdraw());
-//        assertFalse(result.isIgnore());
-//        assertFalse(result.isForeign());
-//        assertEquals(0, result.getMiles().compareTo(new BigDecimal("4")));
-//
-//    }
-
     @Test
-    public void testForeign() {
+    public void testForeignByCurrencyCode() {
         transaction.setCurrencyCode("USD");
-        transaction.setAmountInAccountCurrency(new BigDecimal("-100.0"));
         RewardResult result = rule.process(Collections.singletonList(transaction));
         List<TransactionRewardResult> foreignTransactions = result.getTransactionsMap().get(Transaction.Type.WITHDRAW_FOREIGN);
 
         assertEquals(1, foreignTransactions.size());
-
     }
 
-//    @Test
-//    public void testForeignByDescription() {
-//        transaction.setDescription("my foreign transaction");
-//        transaction.setAmountInAccountCurrency(new BigDecimal("-100.0"));
-//        RewardResult result = rule.process(Collections.singletonList(transaction));
-//
-//        assertEquals(0, result.getMiles().compareTo(new BigDecimal("5")));
-//
-//    }
+    @Test
+    public void testForeignByDescription() {
+        transaction.setDescription("my foreign transaction");
+        RewardResult result = rule.process(Collections.singletonList(transaction));
+        List<TransactionRewardResult> foreignTransactions = result.getTransactionsMap().get(Transaction.Type.WITHDRAW_FOREIGN);
+
+        assertEquals(1, foreignTransactions.size());
+    }
 
     @Test
     public void testIgnoreByDescription() {
         transaction.setDescription("Ignore this transaction, please ignore");
         RewardResult result = rule.process(Collections.singletonList(transaction));
 
+        List<TransactionRewardResult> foreignTransactions = result.getTransactionsMap().get(Transaction.Type.IGNORE);
         assertEquals(0, result.getTotalRewardMiles().compareTo(new BigDecimal("0")));
     }
 }
