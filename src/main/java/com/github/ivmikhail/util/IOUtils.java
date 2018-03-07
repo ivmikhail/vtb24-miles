6cc28e4876e15224d829e5e696ad98350a277d20
Mikhail Ivanov <ivmikhail@gmail.com>
2018-03-07 17:59:16 +0300

    Make util class final

--- src/main/java/com/github/ivmikhail/util/IOUtils.java
+++ src/main/java/com/github/ivmikhail/util/IOUtils.java
@@ -3,7 +3,7 @@ package com.github.ivmikhail.util;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class IOUtils {
+public final class IOUtils {
     private IOUtils() { /* util class */}
 
     public static void close(InputStream is) {
