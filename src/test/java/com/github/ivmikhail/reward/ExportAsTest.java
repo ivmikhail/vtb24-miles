0b66e552613ab250bbf0fadd2a582dc3e1899bfe
Mikhail Ivanov <ivmikhail@gmail.com>
2018-03-07 14:46:37 +0300

    ExportAsTest improvements

--- src/test/java/com/github/ivmikhail/reward/ExportAsTest.java
+++ src/test/java/com/github/ivmikhail/reward/ExportAsTest.java
@@ -1,19 +1,21 @@
 package com.github.ivmikhail.reward;
 
 import com.github.ivmikhail.app.Settings;
+import com.github.ivmikhail.transactions.Transaction;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class ExportAsTest {
 
@@ -21,12 +23,30 @@ public class ExportAsTest {
 
     @Before
     public void setUp() {
+        Transaction t = new Transaction();
+        t.setAmount(new BigDecimal("-100.0"));
+        t.setAmountInAccountCurrency(new BigDecimal("-100.0"));
+        t.setDescription("Test transaction");
+        t.setCurrencyCode("RUR");
+        t.setAccountCurrencyCode("RUR");
+        t.setProcessedDate(LocalDate.MIN);
+        t.setDateTime(LocalDateTime.MIN);
+        t.setAccountNumberMasked("12**34");
+        t.setStatus("Обработано");
+
+
+        TransactionReward tr = new TransactionReward();
+        tr.setMiles(new BigDecimal("4.00"));
+        tr.setTransactionType(Transaction.Type.WITHDRAW_NORMAL);
+        tr.setTransaction(t);
+
         Settings s = new Settings();
         s.setMinDate(LocalDate.MIN);
         s.setMaxDate(LocalDate.MAX);
 
         rewardResult = new RewardResult();
         rewardResult.setSettings(s);
+        rewardResult.add(tr);
     }
 
     @Test
@@ -36,13 +56,15 @@ public class ExportAsTest {
 
         List<String> lines = Files.readAllLines(csv.toPath(), StandardCharsets.UTF_8);
         String lastLine = lines.get(lines.size() - 1).replace("\"", "");
-        assertEquals("Эффективный кэшбек %    0", lastLine);
+        assertEquals("Эффективный кэшбек %    4.00", lastLine);
     }
 
     @Test
     public void testExportTxt() {
         String result = ExportAs.txt(rewardResult);
+        String lines[] = result.split("\\r?\\n");
+        String lastLine = lines[lines.length - 1];
 
-        assertTrue(result.endsWith("Эффективный кэшбек %    0"));
+        assertEquals("Эффективный кэшбек %    4", lastLine);
     }
 }
